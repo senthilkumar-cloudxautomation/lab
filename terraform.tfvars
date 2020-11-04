9d594b965c703a34355f31d1da8cb822d7a3c280
Senthil Kumar <senthilkumar@cloudxautomation.com>
2020-11-04 21:47:35 +0530

    c

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+web_server_location       = "westus2"
+web_server_rg             = "web-rg-test"
+resource_prefix           = "web-server"
+web_server_address_space  = "1.0.0.0/22"
+web_server_address_prefix = "1.0.1.0/24"
+web_server_name           = "web-01"
---
+++ vars.tf
@@ -0,0 +1,29 @@
+variable "web_server_location" {
+    type = string
+ 
+}
+
+variable "web_server_rg" {
+    type = string
+   
+}
+
+variable "resource_prefix" {
+    type = string
+   
+}
+
+variable "web_server_address_space" {
+    type = string
+  
+}
+
+variable "web_server_address_prefix" {
+    type = string
+    
+}
+
+variable "web_server_name" {
+    type = string
+
+}
